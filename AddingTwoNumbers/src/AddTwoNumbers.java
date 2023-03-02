289e9620ecb5367af88122f644237e7e90ed1454
Kilian <kilian.mader@hotmail.de>
2023-03-02 21:12:47 +0100

    remove import scanner statement

--- AddingTwoNumbers/src/AddTwoNumbers.java
+++ AddingTwoNumbers/src/AddTwoNumbers.java
@@ -1,13 +1,9 @@
-import java.util.Scanner;
-
 public class AddTwoNumbers {
 
 	public static void main(String[] args) {
 
 		float x = 5;
-		float z = 10;
-		
-		//int sum = x+z;
+		float z = 10;	
 		
 		System.out.printf("The result of the two numbers is: %.2f ", x+z);
 		
