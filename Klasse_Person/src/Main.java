d900af00425f317ce9668e15164eab38d879f79a
Kilian Mader <kilian.mader@hotmail.de>
2023-03-20 12:50:38 +0100

    add code in main method to create a person object and print name and vorname value

--- Klasse_Person/src/Main.java
+++ Klasse_Person/src/Main.java
@@ -2,7 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Person xy = new Person();
+	
+		xy.setVorname("Simon");
+		xy.setName("Simgri");
+		
+		
+		System.out.println("Vorname: " + xy.getVorname() + System.lineSeparator() + "Nachname: " + xy.getName());
+		
 
 	}
 
