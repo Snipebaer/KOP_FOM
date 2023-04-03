0a9a894695a112b1e3f21d986fda4135d9487b7a
Kilian Mader <kilian.mader@hotmail.de>
2023-04-03 14:40:46 +0200

    solved exercise 3 & 4

--- Teil6/src/aufgabe1/ZahlenVergleich.java
+++ Teil6/src/aufgabe1/ZahlenVergleich.java
@@ -6,23 +6,23 @@ public class ZahlenVergleich
 {
    public static void main(String[] args)
    {
-//      Scanner scan = . . . . .;
-//      System.out.print ("Gib die erste Zahl ein: ");
-//      int zahl1 = . . . . .;
-//      System.out.print ("Gib die zweite Zahl ein: ");
-//      int zahl2 = . . . . .;
+      Scanner scan = new Scanner(System.in);
+      System.out.print ("Gib die erste Zahl ein: ");
+      int zahl1 = scan.nextInt();
+      System.out.print ("Gib die zweite Zahl ein: ");
+      int zahl2 = scan.nextInt();
 
-//      if (. . . . .)
-//      {
-//         . . . . .
-//      } 
-//      else if (. . . . .)
-//      {
-//         . . . . .
-//      }
-//      else
-//      {
-//         . . . . .
-//      }
+      if (zahl1 == zahl2)
+      {
+    	  System.out.print ("Die Zahlen sind gleich!");
+      } 
+      else if (zahl1 > zahl2)
+      {
+    	  System.out.print ("Die Zahl " + zahl1 + " ist größer als " + zahl2);
+      }
+      else
+      {
+    	  System.out.print ("Die Zahl " + zahl2 + " ist größer als " + zahl1);
+      }
    }
 }
