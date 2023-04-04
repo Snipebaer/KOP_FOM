5af69419401cea749ba239d337e84f22c1df71f5
Kilian Mader <kilian.mader@hotmail.de>
2023-04-04 09:41:50 +0200

    add solution for exercise 3

--- Teil7/src/aufgabe1/AddInteger.java
+++ Teil7/src/aufgabe1/AddInteger.java
@@ -3,8 +3,21 @@ package aufgabe1;
 public class AddInteger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		if(args.length == 0)
+		{
+			throw new IndexOutOfBoundsException("No arguments passed!");			
+		}
+		
+		int zahl1 = Integer.parseInt(args[0]);
+		int zahl2 = Integer.parseInt(args[1]);
+		int summe = zahl1+zahl2;
+		
+		System.out.println(zahl1 + " + " + zahl2 + " = " + summe);
+		
+	       
+		
+		
 	}
 
 }
