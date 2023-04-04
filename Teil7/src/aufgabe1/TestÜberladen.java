e8ec90d2a1583820d22be6a419af1af746120609
Kilian Mader <kilian.mader@hotmail.de>
2023-04-04 09:15:54 +0200

    add TestÜberladen.java exercise 2

---
+++ Teil7/src/aufgabe1/TestÜberladen.java
@@ -0,0 +1,18 @@
+package aufgabe1;
+
+public class TestÜberladen {
+
+	public static void main(String[] args) {
+
+		boolean x = true;
+		int z = 10;
+		String johannes = "Krausse";
+		
+		System.out.println();
+		System.out.println(x);
+		System.out.println(z);
+		System.out.println(johannes);
+
+	}
+
+}
