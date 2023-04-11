5a21b53d1136e73d10e953a95fa63e43d201ce9d
Kilian Mader <kilian.mader@hotmail.de>
2023-04-11 09:20:40 +0200

    add 'Person' and 'TestPerson' aus Teil5

---
+++ Teil8/src/aufgabe2/Person.java
@@ -0,0 +1,56 @@
+package aufgabe2;
+
+public class Person {
+	
+	private static int anzahlPersonen;
+	
+	private String vorname;
+	private String name;
+	
+	public Person()
+	{
+		
+	}
+	
+	public Person(String name, String vorname)
+	{
+		
+		this.name = name;
+		this.vorname = vorname;
+		
+		anzahlPersonen++;
+	}
+	
+	public static int getAnzahlPersonen()
+	{
+		return anzahlPersonen;
+	}
+	
+	// Methode um den Namen zu setzen
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	// Methode um den Vornamen zu setzen
+	public void setVorname(String vorname)
+	{
+		this.vorname = vorname;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getVorname()
+	{
+		return vorname;
+	}
+	
+	public void printCredentials()
+	{
+		System.out.println("Vorname: " + this.vorname + System.lineSeparator() + "Nachname: " + this.name);
+		System.out.println("----------");
+	}
+}
