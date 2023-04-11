f13c65b3d2f78645b014d46c6fa12e338cfeb865
Kilian Mader <kilian.mader@hotmail.de>
2023-04-11 08:38:03 +0200

    add properties and constructor

--- Teil8/src/aufgabe1/Buch.java
+++ Teil8/src/aufgabe1/Buch.java
@@ -1,5 +1,22 @@
 package aufgabe1;
 
 public class Buch {
+	
+	private String titel;
+	private String isbn;
+	private int anzahlExemplare;
+	
+	public Buch()
+	{
+		
+	}
+	
+	public Buch(String titel, String isbn, int anzahlExemplare)
+	{
+		this.titel = titel;
+		this.isbn = isbn;
+		this.anzahlExemplare = anzahlExemplare;
+	}
+	
 
 }
