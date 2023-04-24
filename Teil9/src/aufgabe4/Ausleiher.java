9a2ea2fb6dc232573430a0805d0817bb44caa910
Kilian Mader <kilian.mader@hotmail.de>
2023-04-24 14:22:07 +0200

    solve exercise 4 and 5

---
+++ Teil9/src/aufgabe4/Ausleiher.java
@@ -0,0 +1,45 @@
+package aufgabe4;
+
+public class Ausleiher extends Person{
+	
+	private int ausleiherNummer;
+	
+	public Ausleiher(String vorname, String nachname)
+	{
+		this(vorname, nachname, 0);
+	}
+	
+	public Ausleiher(String vorname, String nachname, int ausleiherNummer)
+	{
+		super(vorname, nachname);	
+		this.ausleiherNummer = ausleiherNummer;
+	}
+	
+	public int getAusleihernummer()
+	{
+		return this.ausleiherNummer;
+	}
+	
+	public void setAusleihernummer(int ausleiherNummer)
+	{
+		this.ausleiherNummer =  ausleiherNummer;
+	}
+	
+	public void ausgeben()
+	{
+		super.ausgeben();
+		
+		System.out.println("Hadwa");
+	}
+	
+	public void printCredentials()
+	{
+		super.printCredentials();
+		
+		System.out.println("Ausleihernummer: " + this.ausleiherNummer);
+		System.out.println("----------");
+	}
+	
+	
+
+}
---
+++ Teil9/src/aufgabe4/TestAusleiher.java
@@ -0,0 +1,18 @@
+package aufgabe4;
+
+public class TestAusleiher {
+
+	public static void main(String[] args) {
+
+		Person refPerson = new Ausleiher("Max", "Mustermann", 100);
+//		Ausleiher refAusleiher = (Ausleiher) refPerson;
+		
+		refPerson.ausgeben();	
+//		refAusleiher.printCredentials();
+		
+		
+		
+
+	}
+
+}
