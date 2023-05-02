bfb54c1785e081dad390a7f2ba17a9604bd7a7c0
Kilian Mader <kilian.mader@hotmail.de>
2023-05-02 08:57:38 +0200

    add aufgabe7

---
+++ Teil9/src/aufgabe7/TestAusleiher.java
@@ -0,0 +1,21 @@
+package aufgabe7;
+
+public class TestAusleiher {
+
+	public static void main(String[] args) {
+
+//		Person refPerson = new Ausleiher("Max", "Mustermann", 100);
+//		Ausleiher refAusleiher = (Ausleiher) refPerson;
+		
+//		refPerson.ausgeben();	
+//		refAusleiher.printCredentials();
+		
+		Ausleiher a1 = new Ausleiher("Mustermann", "Max", 5);
+		System.out.println(a1.toString());
+		
+		
+		
+
+	}
+
+}
