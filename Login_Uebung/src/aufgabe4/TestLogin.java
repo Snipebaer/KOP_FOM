d7d8da31a8588c679a27784cadfb9643181dedf0
Kilian Mader <kilian.mader@hotmail.de>
2023-05-08 13:13:19 +0200

    add TestLogin class with JUnit

--- Login_Uebung/src/aufgabe4/TestLogin.java
+++ Login_Uebung/src/aufgabe4/TestLogin.java
@@ -5,5 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TestLogin {
+	
+	
+	
+	void BenutzerAnmelden()
+	{
+		Login l1 = new Login("Simon", "1234");
+		
+		
+		assertEquals(null, null);
+	}
 
 }
