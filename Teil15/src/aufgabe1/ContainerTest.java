41426cb631b951d5ea84299c90539c9aa5a892f8
Kilian Mader <kilian.mader@hotmail.de>
2023-06-13 13:34:00 +0200

    add simple solution for exercise 1

--- Teil15/src/aufgabe1/ContainerTest.java
+++ Teil15/src/aufgabe1/ContainerTest.java
@@ -1,9 +1,40 @@
 package aufgabe1;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class ContainerTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame("Hauptfenster");
+		
+		JLabel lblName = new JLabel("Name");
+		
+		JButton btnSubmit = new JButton("Submit");
+		
+		JTextField txtName = new JTextField("Hier könnte Ihre Werbung stehen", 20);
+		//txtName.setSize(new Dimension(50, 50));
+		
+		// Hauptfenster
+		frame.setLayout(new FlowLayout());
+		frame.setSize(500, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Label
+		frame.add(lblName);	
+
+		// Textfeld
+		frame.add(txtName);
+		
+		// Button
+		frame.add(btnSubmit);	
+		
+		// Haupfenster sichtbar machen
+		frame.setVisible(true);
+		
+		
 
 	}
 
